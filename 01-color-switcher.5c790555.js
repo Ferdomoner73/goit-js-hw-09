8d462720dc4b147d961d62cd4f945065313cab94
Ferdomoner73 <122737919+Ferdomoner73@users.noreply.github.com>
2023-06-25 14:09:40 +0000

    Deploying to gh-pages from @ Ferdomoner73/goit-js-hw-09@ad31cdf38dd0615f77c9be5eb2edfb637612e0f9 🚀

---
+++ 01-color-switcher.5c790555.js
@@ -0,0 +1,2 @@
+const t={buttonStartEl:document.querySelector("button[data-start]"),buttonStopEl:document.querySelector("button[data-stop]"),bodyEl:document.querySelector("body")};let e=null;t.buttonStartEl.addEventListener("click",(()=>{e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.buttonStartEl.setAttribute("disabled","disabled")})),t.buttonStopEl.addEventListener("click",(()=>{clearInterval(e),t.buttonStartEl.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.5c790555.js.map
